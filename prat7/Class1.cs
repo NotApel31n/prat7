4a8c3abc4179f0975554a3db9bae79c73668144b
apel31n <isip_d.t.hatamov@mpt.ru>
2023-02-08 22:51:31 +0300

    Добавьте файлы проекта.

---
+++ prat7/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata;
+
+namespace pr7
+{
+    internal class pamparam
+    {
+        static void Main()
+        {
+            do
+            {
+                ex.Final();
+            } while (true);
+        }
+    }
+}
